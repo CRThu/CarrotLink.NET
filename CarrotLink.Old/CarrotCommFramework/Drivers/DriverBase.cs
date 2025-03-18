e7817baf801cac88adac4719a20973aa1ce08356
Carrot <11172391+CRThu@users.noreply.github.com>
2025-03-18 19:55:33 +0800

    add codes from MeasureApp

---
+++ CarrotLink.Old/CarrotCommFramework/Drivers/DriverBase.cs
@@ -0,0 +1,21 @@
+﻿using CarrotCommFramework.Streams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotCommFramework.Drivers
+{
+    public interface IDriver
+    {
+        public string Name { get; set; }
+        public DeviceInfo[] FindDevices();
+    }
+
+    public abstract class DriverBase : IDriver
+    {
+        public string Name { get; set; }
+        public abstract DeviceInfo[] FindDevices();
+    }
+}
---
+++ CarrotLink.Old/CarrotCommFramework/Loggers/NLogLogger.cs
@@ -0,0 +1,20 @@
+﻿using CarrotCommFramework.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotCommFramework.Loggers
+{
+    public class NLogLogger : LoggerBase
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public NLogLogger() : base()
+        {
+
+        }
+    }
+}
