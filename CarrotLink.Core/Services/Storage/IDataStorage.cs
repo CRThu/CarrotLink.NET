61ec0b59347be18738faab817d8d51137f7e4872
Carrot <11172391+CRThu@users.noreply.github.com>
2025-03-20 14:36:38 +0800

    storage重构

---
+++ CarrotLink.Core/Services/Storage/IDataStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotLink.Core.Services.Storage
+{
+    public interface IDataStorage
+    {
+        void StoreInMemory(byte[] data);
+        Task ExportAsJsonAsync(string filePath, object data);
+        Task ExportAsCsvAsync(string filePath, IEnumerable<object> records);
+    }
+}
---
+++ CarrotLink.Core/Services/Storage/MemoryStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotLink.Core.Services.Storage
+{
+    public class MemoryStorage : IDataStorage
+    {
+        private readonly ConcurrentQueue<byte[]> _buffer = new();
+
+        public void StoreInMemory(byte[] data) => _buffer.Enqueue(data);
+
+        public Task ExportAsJsonAsync(string filePath, object data)
+        => throw new NotImplementedException();
+
+        public Task ExportAsCsvAsync(string filePath, IEnumerable<object> records)
+        => throw new NotImplementedException();
+    }
+}
