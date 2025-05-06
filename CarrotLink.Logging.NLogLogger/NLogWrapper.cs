fa265440eb2b25a4fb534de2f0a8348de76e2b39
Carrot <11172391+CRThu@users.noreply.github.com>
2025-05-06 17:05:05 +0800

    nlog

---
+++ CarrotLink.Logging.NLogLogger/NLogWrapper.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+
+namespace CarrotLink.Logging.NLogLogger
+{
+    public class NLogWrapper : CarrotLink.Core.Logging.ILogger
+    {
+        private readonly NLog.Logger _logger;
+
+        public NLogWrapper(bool toconsole = false, string? tofile = default)
+        {
+            NLog.LogManager.Setup().LoadConfiguration(builder =>
+            {
+                if (toconsole)
+                {
+                    builder.ForLogger().FilterMinLevel(LogLevel.Trace).WriteToConsole();
+                }
+                if (tofile != default)
+                {
+                    builder.ForLogger().FilterMinLevel(LogLevel.Trace).WriteToFile(tofile);
+                }
+            });
+
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(string message, Exception? ex = null)
+        {
+            _logger.Error(ex, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+    }
+}
